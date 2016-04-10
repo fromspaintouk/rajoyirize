34b6af6b6ef248c3f8ed74517d7cbab8bb17e778
Jesus Iglesias <lochus@gmail.com>
2016-04-10 11:44:59 +0100

    Updated quotes file

--- assets/quotes.js
+++ assets/quotes.js
@@ -6,7 +6,7 @@ var quotes = [
         quote: 'Porque después del año 14 viene el año 15'
     },
     {
-        quote: 'España es un gran país y tiene españoles'
+        quote: 'España es un gran país que hace cosas importantes y tiene españoles'
     },
     {
         quote: 'Las decisiones se toman en el momento de tomarse'
@@ -45,7 +45,7 @@ var quotes = [
         quote: 'La EPA de mañana será la que conoceremos mañana'
     },
     {
-        quote: 'Lo que no van a hacer nunca las máquinas es fabricar máquinas'
+        quote: 'Tenemos que fabricar máquinas que nos permitan seguir fabricando máquinas porque lo que no va a hacer nunca la máquina es fabricar máquinas'
     },
     {
         quote: 'No es lo mismo que gobierne uno que gobierne otro, no es lo mismo. Dicho de otra forma: es muy distinto, muy diferente'
@@ -57,6 +57,57 @@ var quotes = [
         quote: 'Somos sentimientos y tenemos seres humanos'
     },
     {
-        quote: 'Afirmación RUIZ, ruin, mezquina y miserable'
-    }
+        quote: 'Afirmación Ruiz, ruin, mezquina y miserable'
+    },
+    {
+        quote: 'Yo estoy muy tranquilo. A veces siento la necesidad de gritar, eso es todo'
+    },
+    {
+        quote: 'El presidente del gobierno puede lo que puede, que no es demasiado'
+    },
+    {
+        quote: 'Lo más importante que se puede hacer por vosotros es lo que vosotros podáis hacer por vosotros'
+    },
+    {
+        quote: 'No podemos gastar más de lo que tenemos porque entonces lo tenemos que pedir prestado'
+    },
+    {
+        quote: 'Vivo en el lío'
+    },
+    {
+        quote: 'Los acuerdos que se alcanzan se cumplen'
+    },
+    {
+        quote: 'No dejaré que me digan lo que tengo y lo que no tengo que recortar'
+    },
+    {
+        quote: 'Dije que bajaría los impuestos y los estoy subiendo'
+    },
+    {
+        quote: 'Todo lo que se ha publicado es falso, salvo alguna cosa, que es lo que han publicado los medios de comunicación'
+    },
+    {
+        quote: 'Por las carreteras tienen que ir coches y de los aeropuertos tienen que salir aviones'
+    },
+    {
+        quote: 'Para mí, ser presidente del país es la pera'
+    },
+    {
+        quote: 'Exportar es positivo porque vendes lo que produces'
+    },
+    {
+        quote: 'Estoy dispuesto a seguir dando guerra en la buena dirección'
+    },
+    {
+        quote: 'Cada mañana para desayunar me como un kiwi'
+    },
+    {
+        quote: 'It’s very difficult…todo esto'
+    },
+    {
+        quote: '¿Y la europea?'
+    },
+    {
+        quote: 'Lo que nosotros hemos hecho, cosa que no hizo usted, es engañar a la gente'
+    }    
 ];
