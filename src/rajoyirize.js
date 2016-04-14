78aa9f5a434006589699a547e0db9eeb38e90217
Albert Plana <albert.plana89@gmail.com>
2016-04-14 21:11:24 +0100

    Start using image

--- src/rajoyirize.js
+++ src/rajoyirize.js
@@ -1,32 +1,51 @@
 (function () {
     'use strict';
 
+    function getRandomQuote(numberOfQuotes) {
+        var randomIndex = Math.floor((Math.random() * numberOfQuotes + 1) - 1);
+        return randomIndex;
+    }
+
     var Rajoyirize = function (settings) {
         settings = settings || {};
-        this.autostart = settings.autostart || false;
-        this.rajoyirizeContainer = settings.containerId || 'rajoyirize-container';
+        this.rajoyirizeContainerID = settings.containerId || 'rajoyirize-container';
+        this.rajoyImageSrc = settings.imageSrc || '/assets/images/rajoy/rajoy.png';
         this.quotes = this.getQuotes() || [];
+        this.autostart = settings.autostart || false;
+
+        this.initElements();
 
         if (this.autostart) {
             this.start();
         }
     };
 
-    function getRandomQuote(numberOfQuotes) {
-        var randomIndex = Math.floor((Math.random() * numberOfQuotes + 1) - 1);
-        return randomIndex;
-    }
+    Rajoyirize.prototype.initElements = function () {
+        this.rajoyirizeContainer = document.createElement('div');
+        this.rajoyirizeContainer.id = this.rajoyirizeContainerID;
+        this.rajoyirizeContainer.className = 'hidden';
 
-    Rajoyirize.prototype.displayQuote = function (numberOfQuotes) {
-        var quoteContainer = document.getElementById(this.rajoyirizeContainer);
-        var randomQuote = this.quotes[getRandomQuote(numberOfQuotes)].quote;
-        quoteContainer.textContent = randomQuote;
+        this.rajoy = document.createElement('img');
+        this.rajoy.src = this.rajoyImageSrc;
+        this.rajoy.width = this.rajoy.height = 250;
+
+        this.quoteContainer = document.createElement('p');
+
+        this.rajoyirizeContainer.appendChild(this.rajoy);
+        this.rajoyirizeContainer.appendChild(this.quoteContainer);
     };
 
     Rajoyirize.prototype.start = function () {
         var numberOfQuotes = this.quotes.length;
-        document.body.innerHTML += "<div id='" + this.rajoyirizeContainer + "'></div>";
+
+        document.body.appendChild(this.rajoyirizeContainer);
         this.displayQuote(numberOfQuotes);
+    };
+
+    Rajoyirize.prototype.displayQuote = function (numberOfQuotes) {
+        var randomQuote = this.quotes[getRandomQuote(numberOfQuotes)].quote;
+
+        this.quoteContainer.innerHTML = randomQuote;
     };
 
     Rajoyirize.prototype.getQuotes = function () {
--- tests/specs/rajoyirize-spec.js
+++ tests/specs/rajoyirize-spec.js
@@ -6,40 +6,75 @@ describe("Initialisation of rajoyirize plugin", function () {
         myRajoy = new Rajoyirize();
     });
 
-    it("should retrieve quotes Json object", function () {
+    afterEach(function () {
+        myRajoy = undefined;
+    });
+
+    it("should set default Rajoyirize element container ID", function () {
+        expect(myRajoy.rajoyirizeContainerID).toBe('rajoyirize-container');
+    });
+
+    it("should set default image source", function () {
+        expect(myRajoy.rajoyImageSrc).toBe('/assets/images/rajoy/rajoy.png');
+    });
+
+    it("should retrieve quotes JSON object", function () {
         expect(myRajoy.quotes.length > 0).toBeTruthy();
     });
-    
-    it("should set default Rajoyirize element container", function () {
-        expect(myRajoy.rajoyirizeContainer).toBe('rajoyirize-container');
-    });
-    
+
     it("should set autostart off", function () {
         expect(myRajoy.autostart).toBeFalsy();
     });
-    
+
     describe("On start plugin", function () {
         var testContainerId = 'test-container';
-        
+        var testImageSrc = 'path/to/the/image';
+
         beforeEach(function () {
             myRajoy = new Rajoyirize({
                 autostart: true,
-                containerId: testContainerId
+                containerId: testContainerId,
+                imageSrc: testImageSrc
             });
         });
-        
+
+        afterEach(function () {
+            myRajoy = undefined;
+        });
+
         it("should load plugin options into settings", function () {
             expect(myRajoy.autostart).toBeTruthy();
-            expect(myRajoy.rajoyirizeContainer).toBe(testContainerId);
+            expect(myRajoy.rajoyirizeContainerID).toBe(testContainerId);
+            expect(myRajoy.rajoyImageSrc).toBe(testImageSrc);
         });
-        
+
         it("should create container element and add it to the html", function () {
-            expect(document.getElementById(testContainerId)).not.toBeNull();
+            var rajoyirizeContainer = document.getElementById(testContainerId);
+
+            expect(rajoyirizeContainer).not.toBeNull();
+            expect(rajoyirizeContainer.id).toEqual(testContainerId);
+            expect(rajoyirizeContainer.className).toContain('hidden');
         });
-        
+
+        it("should create an image element and add it to the container", function () {
+            var rajoyirizeContainer = document.getElementById(testContainerId);
+            var rajoyirizeImage = rajoyirizeContainer.getElementsByTagName('img')[0];
+
+            expect(rajoyirizeImage).not.toBeNull();
+            expect(rajoyirizeImage.width).toEqual(250);
+            expect(rajoyirizeImage.height).toEqual(250);
+            expect(rajoyirizeImage.src).toContain(testImageSrc);
+        });
+
+        it("should create a text element and add it to the container", function () {
+            var rajoyirizeContainer = document.getElementById(testContainerId);
+            var rajoyirizeQuote = rajoyirizeContainer.getElementsByTagName('p')[0];
+
+            expect(rajoyirizeQuote).not.toBeNull();
+        });
+
         it("should load a random quote into the container", function () {
             expect(document.getElementById(testContainerId).innerHTML).not.toBe('');
         });
     });
-
 });
