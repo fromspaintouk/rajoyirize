50232e83cac3351424aec8695f059e6b8b4d87b2
Albert Plana <albert.plana89@gmail.com>
2016-04-14 19:35:22 +0100

    Wrap code in a IIFE

--- src/rajoyirize.js
+++ src/rajoyirize.js
@@ -1,36 +1,37 @@
-var Rajoyirize = function (settings) {
+(function () {
     'use strict';
-    settings = settings || {};
-    this.autostart = settings.autostart || false;
-    this.rajoyirizeContainer = settings.containerId || 'rajoyirize-container';
-    this.quotes = this.getQuotes() || [];
 
-    if (this.autostart) {
-        this.start();
+    var Rajoyirize = function (settings) {
+        settings = settings || {};
+        this.autostart = settings.autostart || false;
+        this.rajoyirizeContainer = settings.containerId || 'rajoyirize-container';
+        this.quotes = this.getQuotes() || [];
+
+        if (this.autostart) {
+            this.start();
+        }
+    };
+
+    function getRandomQuote(numberOfQuotes) {
+        var randomIndex = Math.floor((Math.random() * numberOfQuotes + 1) - 1);
+        return randomIndex;
     }
-};
 
-function getRandomQuote(numberOfQuotes) {
-    'use strict';
-    var randomIndex = Math.floor((Math.random() * numberOfQuotes + 1) - 1);
-    return randomIndex;
-}
+    Rajoyirize.prototype.displayQuote = function (numberOfQuotes) {
+        var quoteContainer = document.getElementById(this.rajoyirizeContainer);
+        var randomQuote = this.quotes[getRandomQuote(numberOfQuotes)].quote;
+        quoteContainer.textContent = randomQuote;
+    };
 
-Rajoyirize.prototype.displayQuote = function (numberOfQuotes) {
-    'use strict';
-    var quoteContainer = document.getElementById(this.rajoyirizeContainer);
-    var randomQuote = this.quotes[getRandomQuote(numberOfQuotes)].quote;
-    quoteContainer.textContent = randomQuote;
-};
+    Rajoyirize.prototype.start = function () {
+        var numberOfQuotes = this.quotes.length;
+        document.body.innerHTML += "<div id='" + this.rajoyirizeContainer + "'></div>";
+        this.displayQuote(numberOfQuotes);
+    };
 
-Rajoyirize.prototype.start = function () {
-    'use strict';
-    var numberOfQuotes = this.quotes.length;
-    document.body.innerHTML += "<div id='" + this.rajoyirizeContainer + "'></div>";
-    this.displayQuote(numberOfQuotes);
-};
+    Rajoyirize.prototype.getQuotes = function () {
+        return quotes || [{ quote: 'no quotes file found'}];
+    };
 
-Rajoyirize.prototype.getQuotes = function () {
-    'use strict';
-    return quotes || [{ quote: 'no quotes file found'}];
-};
+    window.Rajoyirize = Rajoyirize;
+}());
